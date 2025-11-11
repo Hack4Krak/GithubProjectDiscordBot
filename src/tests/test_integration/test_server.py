9aa099486f5c09f6fcaabe49ea496d342fa350eb
Kubaryt <kubastarzyk6554@gmail.com>
2025-11-11 22:50:47 +0100

    feat: Write unit, integration and e2e tests

---
+++ src/tests/test_integration/test_server.py
@@ -0,0 +1,167 @@
+import json
+from typing import Any
+from unittest.mock import patch
+
+from aiohttp import ClientSession
+from fastapi.testclient import TestClient
+
+from src.server import app
+from src.tests.test_unit.test_utils import MockResponse, MockShelf
+from src.utils.utils import generate_signature
+
+test_client = TestClient(app)
+
+
+def test_missing_body():
+    response = test_client.post("/webhook_endpoint", data=None)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Missing request body."}
+
+
+@patch("os.getenv")
+def test_invalid_signature(mock_os_getenv):
+    mock_os_getenv.return_value = "some_secret"
+
+    response = test_client.post(
+        "/webhook_endpoint", data={"mreow": "nya"}, headers={"X-Hub-Signature-256": "invalid_signature"}
+    )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid signature"}
+
+
+@patch("os.getenv")
+def test_missing_signature(mock_os_getenv):
+    mock_os_getenv.return_value = "some_secret"
+
+    response = test_client.post(
+        "/webhook_endpoint",
+        data={"mreow": "nya"},
+    )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Missing signature"}
+
+
+@patch("os.getenv")
+def test_invalid_json(mock_os_getenv):
+    mock_os_getenv.return_value = "some_secret"
+    signature = generate_signature("some_secret", b"invalid_json")
+    response = test_client.post(
+        "/webhook_endpoint",
+        content="invalid_json",
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid JSON data."}
+
+
+@patch("os.getenv")
+def test_missing_projects_v2_item(mock_os_getenv):
+    mock_os_getenv.return_value = "some_secret"
+    signature = generate_signature("some_secret", b'{"not_projects_v2_item": "data"}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"not_projects_v2_item": "data"}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Missing projects_v2_item in payload."}
+
+
+@patch("os.getenv")
+def test_missing_project_node_id(mock_os_getenv):
+    mock_os_getenv.return_value = "some_secret"
+    signature = generate_signature("some_secret", b'{"projects_v2_item": {"skibidi": true}}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"projects_v2_item": {"skibidi": true}}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid project_node_id."}
+
+
+@patch("os.getenv")
+def test_invalid_project_node_id(mock_os_getenv):
+    mock_os_getenv.side_effect = ["some_secret", 33]
+    signature = generate_signature("some_secret", b'{"projects_v2_item": {"project_node_id": 123}}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"projects_v2_item": {"project_node_id": 123}}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid project_node_id."}
+
+
+@patch("os.getenv")
+def test_missing_item_node_id(mock_os_getenv):
+    mock_os_getenv.side_effect = ["some_secret", 123]
+    signature = generate_signature("some_secret", b'{"projects_v2_item": {"project_node_id": 123}}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"projects_v2_item": {"project_node_id": 123}}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Missing item_node_id in payload."}
+
+
+@patch.object(ClientSession, "post")
+@patch("shelve.open")
+@patch("os.getenv")
+def test_could_not_fetch_item_name(mock_os_getenv, mock_shelve_open, mock_post_request):
+    mock_os_getenv.side_effect = ["some_secret", 123, "some_token"]
+    mock_shelve_open.return_value = MockShelf({})
+    mock_post_request.return_value = MockResponse({})
+    signature = generate_signature("some_secret", b'{"projects_v2_item": {"project_node_id": 123, "node_id": "123"}}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"projects_v2_item": {"project_node_id": 123, "node_id": "123"}}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 500
+    assert response.json() == {"detail": "Could not fetch item name."}
+
+
+@patch("shelve.open")
+@patch("os.getenv")
+def test_missing_action(mock_os_getenv, mock_shelve_open):
+    mock_os_getenv.side_effect = ["some_secret", 123]
+    mock_shelve_open.return_value = MockShelf({"123": "Meow"})
+    signature = generate_signature("some_secret", b'{"projects_v2_item": {"project_node_id": 123, "node_id": "123"}}')
+    response = test_client.post(
+        "/webhook_endpoint",
+        content='{"projects_v2_item": {"project_node_id": 123, "node_id": "123"}}',
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Missing action in payload."}
+
+
+@patch.object(ClientSession, "post")
+@patch("shelve.open")
+@patch("os.getenv")
+def test_edited_action(mock_os_getenv, mock_shelve_open, mock_post_request):
+    payload: dict[str, Any] = {
+        "projects_v2_item": {"project_node_id": 123, "node_id": "123"},
+        "action": "edited",
+        "changes": {"field_value": {"field_type": "title"}},
+    }
+    payload: str = json.dumps(payload)
+    mock_os_getenv.side_effect = ["some_secret", 123, "some_token"]
+    mock_shelve_open.return_value = MockShelf({"123": "Meow"})
+    mock_post_request.return_value = MockResponse({})
+    signature = generate_signature(
+        "some_secret",
+        payload.encode("utf-8"),
+    )
+    response = test_client.post(
+        "/webhook_endpoint",
+        content=payload,
+        headers={"X-Hub-Signature-256": signature},
+    )
+    assert response.json() == {"detail": "Successfully received webhook data"}
+    assert response.status_code == 200
+    mock_post_request.assert_called()
---
+++ src/tests/test_unit/test_server.py
@@ -0,0 +1,68 @@
+from unittest.mock import patch
+
+from src.server import process_edition
+from src.utils.data_types import (
+    ProjectItemEditedAssignees,
+    ProjectItemEditedBody,
+    ProjectItemEditedSingleSelect,
+    ProjectItemEditedTitle,
+    SingleSelectType,
+)
+
+
+async def test_process_edition_body_changes():
+    body = {"changes": {"body": {"to": "We need to pet more cats"}}}
+    item_name = "PetSomeCats"
+    expected_object = ProjectItemEditedBody(item_name, "Unknown", "We need to pet more cats")
+
+    assert await process_edition(body, item_name) == expected_object
+
+
+async def test_process_edition_body_no_changes():
+    body = {}
+    item_name = "Idk"
+
+    assert await process_edition(body, item_name) is None
+
+
+@patch("src.server.fetch_assignees")
+async def test_process_edition_assignees_changed(mock_fetch_assignees):
+    body = {"changes": {"field_value": {"field_type": "assignees"}}}
+    item_name = "YouKnowIntegrationTestsAreNextDontYou?"
+    new_assignees = ["Kubaryt", "Salieri", "Aniela"]
+    mock_fetch_assignees.return_value = new_assignees
+    expected_object = ProjectItemEditedAssignees(item_name, "Unknown", new_assignees)
+
+    assert await process_edition(body, item_name) == expected_object
+
+
+@patch("src.server.fetch_item_name")
+async def test_process_edition_title_changed(mock_fetch_item_name):
+    body = {"changes": {"field_value": {"field_type": "title"}}}
+    item_name = "ImagineMockingDiscordSoMuchFun"
+    new_item_name = "ActuallyNotFunAtAll"
+    mock_fetch_item_name.return_value = new_item_name
+    expected_object = ProjectItemEditedTitle(item_name, "Unknown", new_item_name)
+
+    assert await process_edition(body, item_name) == expected_object
+
+
+async def test_process_edition_single_select_changed():
+    body = {
+        "changes": {
+            "field_value": {"field_type": "single_select", "field_name": "Size", "to": {"name": "Smol like lil kitten"}}
+        }
+    }
+    item_name = "Lil puppy"
+    expected_object = ProjectItemEditedSingleSelect(item_name, "Unknown", "Smol like lil kitten", SingleSelectType.SIZE)
+
+    assert await process_edition(body, item_name) == expected_object
+
+
+async def test_process_edition_iteration_changed():
+    new_title = "1.0.0 - FinallyWeShipItAfter25Years"
+    body = {"changes": {"field_value": {"field_type": "iteration", "to": {"title": new_title}}}}
+    item_name = "Create Dockerfile for production"
+    expected_object = ProjectItemEditedSingleSelect(item_name, "Unknown", new_title, SingleSelectType.ITERATION)
+
+    assert await process_edition(body, item_name) == expected_object

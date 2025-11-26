85379b3fd0de2d5838e359b03d83ac97e413a012
Kubaryt <kubastarzyk6554@gmail.com>
2025-11-26 16:33:34 +0100

    refactor: Remove code duplication from utils.github_api

--- src/tests/test_unit/test_utils/test_github_api.py
+++ src/tests/test_unit/test_utils/test_github_api.py
@@ -1,25 +1,21 @@
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 
 import pytest
-from aiohttp import ClientSession
 from fastapi import HTTPException
 
-from src.tests.utils import MockResponse
 from src.utils import github_api
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_item_name_success(mock_post_request):
-    mock_response = {"data": {"node": {"content": {"title": "42"}}}}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_item_name_success(mock_send_request):
+    mock_send_request.return_value = {"data": {"node": {"content": {"title": "42"}}}}
 
     assert await github_api.fetch_item_name("<node_id>") == "42"
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_item_name_partial(mock_post_request):
-    mock_response = {"data": {"node": {"content": None}}}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_item_name_partial(mock_send_request):
+    mock_send_request.return_value = {"data": {"node": {"content": None}}}
 
     with pytest.raises(HTTPException) as exception:
         await github_api.fetch_item_name("<node_id>")
@@ -28,9 +24,9 @@ async def test_fetch_item_name_partial(mock_post_request):
     assert exception.value.detail == "Could not fetch item name."
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_item_name_none(mock_post_request):
-    mock_post_request.return_value = MockResponse({})
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_item_name_none(mock_send_request):
+    mock_send_request.return_value = {}
 
     with pytest.raises(HTTPException) as exception:
         await github_api.fetch_item_name("<node_id>")
@@ -38,54 +34,49 @@ async def test_fetch_item_name_none(mock_post_request):
     assert exception.value.detail == "Could not fetch item name."
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_assignees_success(mock_post_request):
-    mock_response = {
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_assignees_success(mock_send_request):
+    mock_send_request.return_value = {
         "data": {
             "node": {
                 "content": {"assignees": {"nodes": [{"id": "MDQ6VXNlcjg4MjY4MDYz"}, {"id": "MDQ6VXNlcjg5ODM3NzI0"}]}}
             }
         }
     }
-    mock_post_request.return_value = MockResponse(mock_response)
 
     assert await github_api.fetch_assignees("<node_id>") == ["MDQ6VXNlcjg4MjY4MDYz", "MDQ6VXNlcjg5ODM3NzI0"]
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_assignees_partial(mock_post_request):
-    mock_response = {"data": {"node": {"content": None}}}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_assignees_partial(mock_send_request):
+    mock_send_request.return_value = {"data": {"node": {"content": None}}}
 
     assert await github_api.fetch_assignees("<node_id>") == []
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_assignees_none(mock_post_request):
-    mock_post_request.return_value = MockResponse({})
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_assignees_none(mock_send_request):
+    mock_send_request.return_value = {}
 
     assert await github_api.fetch_assignees("<node_id>") == []
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_single_select_value_success(mock_post_request):
-    mock_response = {"data": {"node": {"fieldValueByName": {"name": "Dziengiel"}}}}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_single_select_value_success(mock_send_request):
+    mock_send_request.return_value = {"data": {"node": {"fieldValueByName": {"name": "Dziengiel"}}}}
 
     assert await github_api.fetch_single_select_value("<node_id>", "Salieri") == "Dziengiel"
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_single_select_value_partial(mock_post_request):
-    mock_response = {"data": {"node": {"fieldValueByName": None}}}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_single_select_value_partial(mock_send_request):
+    mock_send_request.return_value = {"data": {"node": {"fieldValueByName": None}}}
 
     assert await github_api.fetch_single_select_value("<node_id>", "Salieri") is None
 
 
-@patch.object(ClientSession, "post")
-async def test_fetch_single_select_value_none(mock_post_request):
-    mock_response = {}
-    mock_post_request.return_value = MockResponse(mock_response)
+@patch("src.utils.github_api.send_request", new_callable=AsyncMock)
+async def test_fetch_single_select_value_none(mock_send_request):
+    mock_send_request.return_value = {}
 
     assert await github_api.fetch_single_select_value("<node_id>", "Salieri") is None
